123487259451e369f8eea3afa43a61a1f9d6764a
Pointer31 <pointer.31.31.31@gmail.com>
2026-03-14 17:03:34 +0100

    disable force-spectate feature for inftile deaths

--- src/game/server/infclass/ic_gamecontroller.cpp
+++ src/game/server/infclass/ic_gamecontroller.cpp
@@ -7195,7 +7195,7 @@ void CIcGameController::OnIcCharacterDeath(CIcCharacter *pVictim, DeathContext *
 		if(pVictim->IsHuman())
 		{
 			const CIcPlayer *pKiller = GetPlayer(pContext->Killer);
-			if(pKiller && pKiller->IsInfected() && pKiller->GetCharacter())
+			if(pKiller && pKiller->IsInfected() && pKiller->GetCharacter() && DamageType != EDamageType::INFECTION_TILE)
 			{
 				pVictim->GetPlayer()->SetSpecialCameraTargetCid(pKiller->GetCid(), 5.0);
 			}
